b6e71cc69aa0e08c8c690a6204bc261346c01259
Jan Nicklas <j.nicklas@me.com>
2020-06-29 10:43:22 +0200

    docs: Update example

--- example/src/index.js
+++ example/src/index.js
@@ -1,8 +1,22 @@
-import { renderMain } from "./article.htl";
+import { render, renderMain } from "./article.htl";
 
 async function start() {
+
   document.body.insertAdjacentHTML(
     "beforeend",
+
+    // Render template data-sly-template.article
+    await render(
+      "article",
+      { name: "Sue", body: "Lorem ipsum" },
+      { "com.foo.core.models.myModel": { salutation: "Hey", end: "The end" } }
+    )
+  );
+
+  document.body.insertAdjacentHTML(
+    "beforeend",
+    
+    // Render the entire file
     await renderMain(
       { name: "Sue", body: "Lorem ipsum" },
       { "com.foo.core.models.myModel": { salutation: "Hey", end: "The end" } }
